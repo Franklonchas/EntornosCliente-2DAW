912b6b5892e3df4a1a5cbebf2ea9d9368e9d78bf
Francisco Javier Sánchez de la Torre <fjsancheztrabajos@gmail.com>
2018-09-25 18:59:33 +0200

    ejercicios

---
+++ Tema 1/Slides 5/slides5js.js
@@ -0,0 +1,44 @@
+// Write a function that can take in any number of arguments, and returns the sum of all of the arguments.
+
+var args = [1, 2, 3, 4, 5];
+var args2 = [6, 7, 8, 9, 10];
+
+function argSuma(...args) {
+    let cont = 0;
+    for (let i = 0; i < args.length; i++) {
+        cont += args[i];
+    }
+    return cont;
+}
+
+/*Write a function called addOnlyNums that can take in any number of arguments (including numbers or strings), and 
+returns the sum of only the numbers.*/
+
+function addOnlyNums(args) {
+    let cont = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] == "number") {
+            cont += args[i];
+        } else {
+        }
+    }
+    return cont;
+}
+
+/*Write a function called combineTwoArrays that takes in two arrays as arguments, and returns a single array that
+combines both (using the spread operator).*/
+
+function combineTwoArrays(array1, array2) {
+    let newArray = [array1, array2];
+    return newArray;
+}
+
+/*Write a function called sumEveryOther that takes in any amount of arguments, and returns the sum of every other argument.*/
+
+function sumEveryOther(args) {
+    let cont = 0;
+    for (let i = 0; i = args.length; i + 2) {
+        cont += args[i];
+    }
+    return cont;
+}
--- Tema 1/WS5/ws5js.js
+++ Tema 1/WS5/ws5js.js
@@ -17,3 +17,109 @@ function crearTablas(ncolumnas, alto, ancho) {
     document.write('</tr>');
     document.write('</table>');
 }
+
+//ejer 12 code
+
+function ejer12(numCol, alto, ancho) {
+    document.write("<table border='0' cellspacing ='2' bgcolor = 'black' width = '200'>");
+    document.write("<tr bgcolor = 'white' height = " + alto + ">");
+    for (let i = 1; i <= numCol; i++) {
+        if (i % 2) {
+            document.write("<td bgcolor = 'black' width= " + ancho + " ></td>");
+        } else {
+            document.write("<td width= " + ancho + " ></td>");
+        }
+    }
+    document.write("</tr>");
+    document.write("</table>");
+}
+
+//ejer 13 code
+
+function ejer13(numCol, alto, ancho) {
+    let cont = 0;
+    document.write("<table border='0' cellspacing ='2' bgcolor = 'black' width = '200'>");
+    document.write("<tr bgcolor = 'white' height = " + alto + ">");
+    while (cont <= numCol) {
+        document.write("<td width= " + ancho + " ></td>");
+        cont++;
+    }
+    document.write("</tr>");
+    document.write("</table>");
+}
+
+//ejer 14 code
+
+function ejer14(numCol, alto, ancho) {
+    var cont = 1;
+    document.write("<table border='0' cellspacing ='2' bgcolor = 'black' width = '200'>");
+    document.write("<tr bgcolor = 'white' height = " + alto + ">");
+    while (cont <= numCol) {
+        if (cont % 2) {
+            document.write("<td bgcolor = 'black' width= " + ancho + " ></td>");
+            cont++;
+        } else {
+            document.write("<td width= " + ancho + " ></td>");
+            cont++;
+        }
+    }
+    document.write("</tr>");
+    document.write("</table>");
+}
+
+//ejer 15 code
+
+function ejer15(numAdivinar) {
+    let num = -1;
+    while (num != numAdivinar) {
+        num = parseInt(prompt("Introduce un numero"));
+        if (num > numAdivinar) {
+            alert("El numero a adivinar es menor");
+        } else if (num < numAdivinar) {
+            alert("El numero a adivinar es mayor");
+        } else {
+            alert("Has acertado!");
+        }
+    }
+}
+
+// ejer 16 code
+
+function ejer16(numAdivinar) {
+    let num = -1;
+    do {
+        num = parseInt(prompt("Introduce un numero"));
+        if (num > numAdivinar) {
+            alert("El numero a adivinar es menor");
+        } else if (num < numAdivinar) {
+            alert("El numero a adivinar es mayor");
+        } else {
+            alert("Has acertado!");
+        }
+    } while (num != numAdivinar)
+}
+
+// ejer 17 code
+
+function ejer17() {
+    for (let y = 1; y <= 10; y++) {
+        document.write("<h3>Tabla del " + y + "</h3>");
+        for (let x = 1; x <= 10; x++) {
+            document.write(y + "x" + x + "=" + y * x + "</br>");
+        }
+    }
+}
+
+// ejer 18 code
+
+function ejer18(numCol, numFil, alto, ancho) {
+    document.write("<table border='0' cellspacing ='2' bgcolor = 'black' width = '200'>");
+    for (let z = 1; z <= numFil; z++) {
+        document.write("<tr bgcolor = 'white' height = " + alto + ">");
+        for (let i = 1; i <= numCol; i++) {
+            document.write("<td width= " + ancho + " > Hola </td>");
+        }
+    }
+    document.write("</tr>");
+    document.write("</table>");
+}
